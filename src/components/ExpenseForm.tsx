8ab4a8e629ebf6ef10ce5cdc4b6d2d261dee88b3
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-03-23 19:04:14 +0100

    created the type for the expenses

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import { categories } from "../data/categories";
 import DatePicker from "react-date-picker";
 import 'react-date-picker/dist/DatePicker.css'
 import 'react-calendar/dist/Calendar.css'
-
-type ValuePiece = Date | null;
-
-type Value = ValuePiece | [ValuePiece, ValuePiece];
+import { DraftExpense, Expense } from "../types";
 
 export default function ExpenseForm() {
+
+    const [expense, setExpense] = useState<DraftExpense>({
+        expenseName: '',
+        amount: 0,
+        category: '',
+        date: new Date()
+    })
+
   return (
     <form action="" className="space-y-5">
       <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">
@@ -24,6 +30,7 @@ export default function ExpenseForm() {
           id="expenseName"
           placeholder="Añade el nombre del gasto"
           className="bg-slate-100 p-2"
+          value={expense.expenseName}
         />
       </div>
 
@@ -37,6 +44,7 @@ export default function ExpenseForm() {
           id="amount"
           placeholder="Añade la cantidad del gasto: ej. 300"
           className="bg-slate-100 p-2"
+          value={expense.amount}
         />
       </div>
 
@@ -44,7 +52,7 @@ export default function ExpenseForm() {
         <label htmlFor="category" className="text-xl">
           Categoría:
         </label>
-        <select id="category" className="bg-slate-100 p-2" name="category">
+        <select id="category" className="bg-slate-100 p-2" name="category" value={expense.category}>
           <option value="">-- Seleccione --</option>
           {categories.map((category) => (
             <option key={category.id} value={category.id}>
@@ -60,6 +68,7 @@ export default function ExpenseForm() {
         </label>
         <DatePicker
             className="bg-slate-100 p-2 border-0"
+            value={expense.date}
         />
       </div>
 
