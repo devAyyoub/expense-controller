8d120c8e086a45ace7fd9f56ca8d4e1a01ad357a
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-03-23 18:01:19 +0100

    installed headlessui to have a modal window

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,24 +1,35 @@
-export type BudgetActions = { type: "add-budget"; payload: { budget: number } };
+export type BudgetActions =
+  | { type: "add-budget", payload: { budget: number } }
+  | { type: "show-modal" };
 
 export type BudgetState = {
   budget: number;
+  modal: boolean
 };
 
 export const initialState: BudgetState = {
   budget: 0,
+  modal: false
 };
 
 export const budgetReducer = (
   state: BudgetState = initialState,
   action: BudgetActions
 ) => {
-  if ((action.type = "add-budget")) {
-
-    console.log(action.payload.budget)
+  
+  if (action.type === "add-budget") {
     return {
       ...state,
-      budget: action.payload.budget
+      budget: action.payload.budget,
     };
   }
+
+  if ( action.type = 'show-modal' ) {
+    return {
+      ...state,
+      modal: true
+    }
+  }
+
   return state;
 };
