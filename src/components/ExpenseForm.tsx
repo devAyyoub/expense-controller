95061cf908daff55c98a2d8546a26ca6ae2f4549
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-03-23 18:46:57 +0100

    added calendar to specify the date of expense

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,4 +1,11 @@
 import { categories } from "../data/categories";
+import DatePicker from "react-date-picker";
+import 'react-date-picker/dist/DatePicker.css'
+import 'react-calendar/dist/Calendar.css'
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 export default function ExpenseForm() {
   return (
@@ -45,6 +52,15 @@ export default function ExpenseForm() {
             </option>
           ))}
         </select>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="amount" className="text-xl">
+          Fecha gasto:
+        </label>
+        <DatePicker
+            className="bg-slate-100 p-2 border-0"
+        />
       </div>
 
       <input
