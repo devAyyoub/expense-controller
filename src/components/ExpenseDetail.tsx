f7b65e0417ec184f803a4719a1909e8ef0926293
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-03-26 01:07:50 +0100

    working on the logic to show the expenses

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -1,11 +1,24 @@
-import { Expense } from "../types"
+import { formatDate } from "../helpers";
+import { Expense } from "../types";
+import AmountDisplay from "./AmountDisplay";
 
 type ExpenseDetailProps = {
-    expense: Expense
-}
+  expense: Expense;
+};
 
-export default function ExpenseDetail({expense}: ExpenseDetailProps) {
+export default function ExpenseDetail({ expense }: ExpenseDetailProps) {
   return (
-    <div>ExpenseDetail</div>
-  )
+    <div className="bg-white shadow-lg p-10 w-full border-b border-gray-200 flex gap-5 items-center">
+      <div></div>
+      <div>
+        <p className="">{expense.expenseName}</p>
+        <p className="text-slate-600 text-sm">
+          {formatDate(expense.date!.toString())}
+        </p>
+      </div>
+      <AmountDisplay
+        amount={expense.amount}
+      />
+    </div>
+  );
 }
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,15 @@
 export function formatCurrency(amout: number) {
     return new Intl.NumberFormat('es-ES', {style: 'currency', currency: 'EUR'}).format(amout)
 }
+
+export function formatDate(dateStr: string) : string {
+    const dateObj = new Date(dateStr)
+    const options : Intl.DateTimeFormatOptions = {
+        weekday : 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    }
+
+    return new Intl.DateTimeFormat('es-ES', options).format(dateObj)
+}
